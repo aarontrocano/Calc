e28a75523caefe80a33633176baf8b7f58e80ef5
aarontrocano <aaron@atrocano.com>
2019-11-21 08:50:01 -0500

    First Commit

---
+++ calc.ps1
@@ -0,0 +1,26 @@
+function Add {
+    [CmdletBinding()]
+    Param($a,$b)
+
+    Out-Null
+}
+function Subtract {
+    [CmdletBinding()]
+    Param($a,$b)
+
+    Out-Null
+}
+
+function Multiply {
+    [CmdletBinding()]
+    Param($a,$b)
+
+    Out-Null
+}
+
+function Divide {
+    [CmdletBinding()]
+    Param($a,$b)
+
+    Out-Null
+}
